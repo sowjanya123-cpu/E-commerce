2248f043d1e781e9080493fe0536ed6d697ed00e
sowjanya123-cpu <2023cs_sowjanyanu_d@nie.ac.in>
2025-09-18 10:46:21 +0530

    intial commit

--- hello.java
+++
@@ -1,7 +0,0 @@
-// Simple Java Program
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Print a message to the console
-        System.out.println("Hello, World!");
-    }
-}
